8ac93211591b1caf3d96405f04458c8f98dcac07
MERYLL <m.alexandre.14@groupe-esigelec.fr>
2016-11-24 17:42:23 +0100

    jeudi

--- app/src/main/java/com/example/simon/dycard/activity_ajoutdestinataire.java
+++ app/src/main/java/com/example/simon/dycard/activity_ajoutdestinataire.java
@@ -1,7 +1,9 @@
 package com.example.simon.dycard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_ajoutdestinataire extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class activity_ajoutdestinataire extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajoutdestinataire);
+    }
+
+    public void ajouterCeDestinataire(View v) {
+        Intent intent = new Intent(activity_ajoutdestinataire.this, activity_destinataire.class);
+        startActivity(intent);
     }
 }

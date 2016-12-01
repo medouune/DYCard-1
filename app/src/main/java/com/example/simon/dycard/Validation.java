54a798bc1decb896ad2acb488589816674f0b7b9
JulieBa <julie.barkian@hotmail.com>
2016-12-01 13:04:24 +0100

    Validation

--- app/src/main/java/com/example/simon/dycard/Validation.java
+++ app/src/main/java/com/example/simon/dycard/Validation.java
@@ -1,7 +1,9 @@
 package com.example.simon.dycard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Validation extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Validation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validation);
+
+    }
+    public void valider(View v){
+        Intent intent = new Intent(this, activity_payment.class);
+        startActivity(intent);
+    }
+    public void modifier(View v) {
+        Intent intent = new Intent(this, activity_choose_form.class);
+        startActivity(intent);
     }
 }

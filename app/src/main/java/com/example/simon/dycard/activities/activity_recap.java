7e023c2a4a8596596dbb40b94a551153e57cead7
sittingbuzz <picard_simon@orange.fr>
2017-01-05 15:54:47 +0100

    Ajout de l'activité permettant de récapituler à l'utilisateur les informations concernant sa commande

---
+++ app/src/main/java/com/example/simon/dycard/activities/activity_recap.java
@@ -0,0 +1,35 @@
+package com.example.simon.dycard.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.simon.dycard.R;
+import com.example.simon.dycard.model.Commande;
+import com.example.simon.dycard.model.User;
+import com.example.simon.dycard.util.MySingleton;
+
+public class activity_recap extends AppCompatActivity {
+
+    private TextView idUser;
+    private ImageView photo;
+    private User user;
+    private Commande commande;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recap);
+
+        idUser = (TextView) findViewById(R.id.activity_recap_idUser);
+        photo = (ImageView) findViewById(R.id.activity_recap_photo);
+
+        user = MySingleton.getInstance(activity_recap.this).getUser();
+        commande = MySingleton.getInstance(activity_recap.this).getCommande();
+
+        idUser.setText(user.getId());
+        photo.setImageBitmap(commande.getPhoto());
+
+    }
+}
